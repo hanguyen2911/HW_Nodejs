6968a7b48d7093b6283fff769a2574b6d5478cf7
hanguyen2911 <75479163+hanguyen2911@users.noreply.github.com>
2021-12-23 14:43:35 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,5 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/users', function(req, res, next) {
-  res.render('users', { title: 'Express' });
-});
 
-router.get('/users/cool', function(req, res, next) {
-  res.render('cool', { title: 'Express' });
-});
 module.exports = router;
