a05b187551b3c6a340f486372d8bedf8e3504824
hanguyen2911 <ha.nguyen22@student.passerellesnumeriques.org>
2021-12-23 14:41:14 +0700

    create router users

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/users', function(req, res, next) {
+  res.render('users', { title: 'Express' });
+});
 
+router.get('/users/cool', function(req, res, next) {
+  res.render('cool', { title: 'Express' });
+});
 module.exports = router;
