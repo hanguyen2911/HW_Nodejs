a59fe06c811af99b831bfabede192cc60f9661cf
hanguyen2911 <ha.nguyen22@student.passerellesnumeriques.org>
2021-12-24 18:47:36 +0700

    create part3

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,6 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' })
 });
-// GET home page.
-router.get('/', function(req, res) {
-  res.redirect('/catalog');
-});
+
 
 module.exports = router;
